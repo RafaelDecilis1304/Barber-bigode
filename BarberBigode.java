c85b79d8cbf5f6939dbc73a8d55923ea73a066be
RafaelDecilis1304 <rafael.deciles123@gmail.com>
2025-11-19 10:19:43 -0300

    Create BarberBigode class with iniciar method
    Implement initial structure for BarberBigode class with iniciar method.

---
+++ BarberBigode.java
@@ -0,0 +1,20 @@
+public class BarberBigode {
+
+    /**
+     * Método responsável por iniciar o sistema da barbearia.
+     * Aqui podem ser adicionadas chamadas para menus,
+     * carregamento de dados, ou inicialização de serviços.
+     */
+    public void iniciar() {
+        
+        // Mensagem exibida quando o sistema é iniciado
+        System.out.println("Sistema iniciado");
+
+        // TODO: implementar lógica principal aqui
+        // Exemplo:
+        // - mostrar menu
+        // - criar lista de agendamentos
+        // - chamar serviços
+        // - solicitar entrada do usuário
+    }
+}
